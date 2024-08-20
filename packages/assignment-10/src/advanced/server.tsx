5cca01863dbb959e4000fe56eb7ced44aedfcfe5
ChangigJeon <wjs1724@naver.com>
2024-08-21 00:28:51 +0900

    feat 심화과제 해결

--- packages/assignment-10/src/advanced/server.tsx
+++ packages/assignment-10/src/advanced/server.tsx
@@ -1,17 +1,42 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react';
-import express from 'express';
-import ReactDOMServer from 'react-dom/server';
-import { App } from './App.tsx';
+import React from "react";
+import express from "express";
+import ReactDOMServer from "react-dom/server";
+import { App } from "./App.tsx";
 
+/**
+ * @NOTE
+ * 방법 1. 자주 사용되는 url을 미리 만들어 캐싱해두고 제공한다.
+ * 방법 2. 한번 들어온 페이지는 캐싱하여 제공한다.
+ */
 const app = express();
 const port = 3333;
 
-app.get('*', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App url={req.url}/>);
+const cache = new Map();
 
-  res.send(`
+const urlsToPreCache = ["/", "/2", "/3", "/4"];
+
+const preCacheUrls = async () => {
+  for (const url of urlsToPreCache) {
+    const rendered = ReactDOMServer.renderToString(<App url={url} />);
+    cache.set(url, rendered);
+  }
+};
+
+preCacheUrls();
+
+app.get("*", (req, res) => {
+  const url = req.url;
+
+  if (cache.has(url)) {
+    console.log(`Serving from cache for ${url}`);
+    return res.send(cache.get(url));
+  }
+
+  const appContent = ReactDOMServer.renderToString(<App url={url} />);
+
+  const html = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -20,10 +45,14 @@ app.get('*', (req, res) => {
       <title>Simple SSR</title>
     </head>
     <body>
-      <div id="root">${app}</div>
+      <div id="root">${appContent}</div>
     </body>
     </html>
-  `);
+  `;
+
+  cache.set(url, html);
+
+  res.send(html);
 });
 
 app.listen(port, () => {
